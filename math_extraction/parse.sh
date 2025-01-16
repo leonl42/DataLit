e47cb2d3695ff491f221f4ab952b32b39c673df5
leonl42 <noinnick8@gmail.com>
2025-01-16 13:24:35 +0100

    finalize parser

--- math_extraction/parse.sh
+++
@@ -1,2 +0,0 @@
-# Absolute path neccessary!!
-python main.py ~/Documents/university/DataLit/math_extraction/extraced_math/07tc5kKRIo ~/Documents/university/DataLit/arXiv_src_fetcher/ICLR_2023_first100/07tc5kKRIo
